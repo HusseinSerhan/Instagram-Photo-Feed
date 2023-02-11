c4a09e383971b92d23f50e6a3ead08fc993cc38b
Hussein Serhan <husseinserhan3012@gmail.com>
2023-02-11 00:44:22 -0500

    Delete Resources/Mohamed Salah (@mosalah) • Instagram photos and videos_files directory

--- Resources/Mohamed Salah (@mosalah) • Instagram photos and videos_files/SozktVQKibX.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("usePolarisPostQuery",["usePolarisQueryStore"],(function(a,b,c,d,e,f,g){"use strict";function a(a,b,d){var e=c("usePolarisQueryStore")();return e.loadPostQuery([a,b],d)}g["default"]=a}),98);
-__d("usePolarisPostQuerySetup",["PolarisQueryBaseHooks"],(function(a,b,c,d,e,f,g){"use strict";function a(a,b){d("PolarisQueryBaseHooks").usePolarisAnyQuerySetup(a,b,"POST")}g["default"]=a}),98);
